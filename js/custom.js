c1ba75330bee88dd0730e9b76d9f9ae376db8722
B-OM Team Member <abhinavdalal@b-om.in>
2017-09-16 15:11:38 +0530

    @dilip - removed errors from index page

--- js/custom.js
+++ js/custom.js
@@ -68,13 +68,13 @@ $(window).load(function () {
          });
       });*/
    // </script>
-   $( document ).ready(function() {
+   /* $( document ).ready(function() {
 
     //##get hash code at next page
     var hashcode = window.location.hash;
 
     // ## move page to any specific position of next page(let that is div with id "hashcode")
-    $('html,body').animate({scrollTop: $('div#'+hascode).offset().top},'slow');
+    $('html,body').animate({scrollTop: $('div#'+hashcode).offset().top},'slow');
 
-    });
+    }); */
 });
