d0a3d8536c990c50e29d2c0ed3d3ad5b029f1668
B-OM Team Member <abhinavdalal@b-om.in>
2017-09-25 17:36:36 +0530

    @vaibhav - removed preloader and with carousel page commited

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 $(window).load(function () {
 
-    // preloader
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(300).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(300).css({
-        'overflow': 'visible'
-    });
+    // // preloader
+    //  $('#status').fadeOut(); // will first fade out the loading animation
+    //  $('#preloader').delay(300).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    //  $('body').delay(300).css({
+    //      'overflow': 'visible'
+    //  });
 
 
     //  isotope
